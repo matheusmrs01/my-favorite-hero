194f33a613be414034198ce5c40daf9c5938aac6
Matheus Morais <matheus.silva.morais01@gmail.com>
2023-06-24 01:50:59 -0300

    feat: criação do endpoint que busca os herois

--- src/hero/hero.controller.ts
+++ src/hero/hero.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { HeroService } from './hero.service';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ParamsDto } from './dto/params.dto';
 
 @Controller('hero')
-export class HeroController {}
+@ApiResponse({
+  status: 400,
+  description: 'Bad Request',
+})
+export class HeroController {
+  constructor(private readonly heroService: HeroService) {}
+
+  @Get(':name')
+  @ApiOperation({
+    operationId: 'listHeroes',
+    summary: 'List all heroes for a given word',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'List of heroes.',
+  })
+  async listHeroes(@Param() params: ParamsDto) {
+    return await this.heroService.listHeroes(params.name);
+  }
+}
