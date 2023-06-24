0bb479e43072045e50d3bd6f6b9a51a61e84333c
Matheus Morais <matheus.silva.morais01@gmail.com>
2023-06-23 23:49:12 -0300

    fix: configurando winston

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestApplicationOptions } from '@nestjs/common';
+import { utilities as nestWinstonUtilities, WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const options = createNestOptions();
+  const app = await NestFactory.create(AppModule, options);
+  app.setGlobalPrefix('api');
   await app.listen(3000);
 }
+
+function createNestOptions(): NestApplicationOptions {
+  const appName = 'my-favorite-hero';
+  const logger = WinstonModule.createLogger({
+    transports: [
+      new winston.transports.Console({
+        format: winston.format.combine(
+          winston.format.timestamp(),
+          nestWinstonUtilities.format.nestLike(appName),
+        ),
+      }),
+    ],
+  });
+
+  return { logger };
+}
+
 bootstrap();
