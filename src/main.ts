ecef2ca4ed4f7cf79c3fff41a679c746ea9e9862
Matheus Morais <matheus.silva.morais01@gmail.com>
2023-06-23 23:56:44 -0300

    fix: configurando swagger

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { NestApplicationOptions } from '@nestjs/common';
+import { INestApplication, NestApplicationOptions } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { utilities as nestWinstonUtilities, WinstonModule } from 'nest-winston';
 import * as winston from 'winston';
 
 async function bootstrap() {
   const options = createNestOptions();
   const app = await NestFactory.create(AppModule, options);
+
   app.setGlobalPrefix('api');
+  setupSwaggerModule(app);
+
   await app.listen(3000);
+}
+
+function setupSwaggerModule(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('My Favorite Hero')
+    .setDescription('Api criada para listar os hérois favoritos da marvel.')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
 }
 
 function createNestOptions(): NestApplicationOptions {
